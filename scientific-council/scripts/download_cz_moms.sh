c817c91a82801db64e03ade0539cb96f94700964
Jan Zeman <jan.zeman4@gmail.com>
2025-11-20 15:24:52 +0100

    Initial commit

---
+++ scientific-council/scripts/download_cz_moms.sh
@@ -0,0 +1,37 @@
+rm -fr tmp/
+mkdir tmp
+
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2025/10/VR_2025-05-20_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2025/10/Usneseni-VR-CVUT_22042025.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2025/05/VR_2025-04-22_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2025/04/VR_2025-01-21_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2025/04/VR_2025-03-25_Z.pdf"
+
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2025/02/VR_2024-12-17_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2024/12/VR_2024-11-26_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2024/12/VR_2024-10-22_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2024/10/VR_2024-06-18_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2024/06/VR_2024-05-21_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2024/05/ENG_VR_2024-03-19_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2024/03/VR_2024_01_23_Zapis.pdf"
+
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2024/02/VR_2023-11-28_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2023/12/VR_2023-10-17_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2023/10/VR_2023-05-23_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2023/05/CZ_VR_2023-04-18_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2023/04/CZ_VR_2023-03-21_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2023/03/VR_2023-2-21_ZapisZ.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2023/02/VR_2023-01-17_Z-2.pdf"
+
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2023/02/VR_2022-11_22_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/11/VR_2022-10-18_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/11/VR_2022-09_20_Zapis.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/07/VR_2022-06-21_ZZ.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/06/VR_2022-05-24_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/06/VR_2022-04-19_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/04/VR_2022-03-22_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/04/VR_2022-02-15_ZS.pdf"
+
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/08/VR_2021-12-14_ZS.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/04/VR_2021-11_23_Z.pdf"
+wget -q -P tmp/ "https://portal.cvut.cz/wp-content/uploads/2022/04/VR_2021-10-05_Z.pdf"
